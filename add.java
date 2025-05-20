c766f47fdc9159ab1516d4a4d2c6a3e241a56dad
SakshiC19 <sakshichougale191@gmail.com>
2025-05-20 18:55:51 +0530

    Added files

---
+++ add.java
@@ -0,0 +1,16 @@
+// Java Program to implement
+// Direct Addition to Add two Numbers
+
+class add {
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    // main function
+    public static void main(String[] args) {
+        add ob = new add();
+        int res = ob.sum(28, 49);
+        System.out.println(res);
+    }
+}
